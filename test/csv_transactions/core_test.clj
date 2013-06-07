1a587edbb33d3d12b379a64b478a36b5fdf7c7a4
Yuriy <Yuriy.Lazarev@gmail.com>
2013-06-07 22:24:47 +0200

    Added test for "aggregate-by-month"

--- test/csv_transactions/core_test.clj
+++ test/csv_transactions/core_test.clj
@@ -4,7 +4,12 @@
 	      [midje.util :only [testable-privates]]))
 
 (testable-privates csv-transactions.core
-	strip-header-footer parse-csv-date parse-number-default parse-csv-rows)
+	strip-header-footer parse-csv-date parse-number-default parse-csv-rows aggregate-by-month)
+
+(defn- dt
+	"Creates org.joda.time.DateTime representing date only"
+	([y m] (dt y m 1))
+	([y m d] (new org.joda.time.DateTime y m d 0 0 org.joda.time.DateTimeZone/UTC)))
 
 (facts "about formatters"
 	(fact "it normally returns the Joda-time formatter"
@@ -17,7 +22,7 @@
 
 (facts "about `parse-csv-date`"
 	(parse-csv-date "05/30/2013") =>
-	  (new org.joda.time.DateTime 2013 5 30 0 0 org.joda.time.DateTimeZone/UTC))
+	  (dt 2013 5 30))
 
 (facts "about `parse-number-default`"
 	(fact "returns default value"
@@ -30,5 +35,13 @@
 (facts "about `parse-csv-rows`"
 	(parse-csv-rows ["03/04/2013;04/05/2013;\"DESC\";-11.80;;EUR",
 	                 "02/26/2013;02/26/2013;\"LOHN/GEHALT\";;5,329.60;EUR"]) =>
-	  [{:date (new org.joda.time.DateTime 2013 3 4 0 0 org.joda.time.DateTimeZone/UTC) :income 0 :expense 11.8}
-		 {:date (new org.joda.time.DateTime 2013 2 26 0 0 org.joda.time.DateTimeZone/UTC) :income 5329.6 :expense 0}])
+	  [{:date (dt 2013 3 4) :income 0 :expense 11.8}
+		 {:date (dt 2013 2 26) :income 5329.6 :expense 0}])
+
+(facts "about `aggregate-by-month`"
+	(aggregate-by-month [{ :date (dt 2013 3 4) :income 2 :expense 5 }
+											 { :date (dt 2013 3 5) :income 3 :expense 6 }
+											 { :date (dt 2013 4 4) :income 4 :expense 7 }]) =>
+	{(dt 2013 3) [{:date (dt 2013 3 4) :income 2 :expense 5}
+	              {:date (dt 2013 3 5) :income 3 :expense 6}]
+	 (dt 2013 4) [{:date (dt 2013 4 4) :income 4 :expense 7}]})
